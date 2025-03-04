17d085e043307244a78fbbfe7af55ff30d80f962
Tomas Godoybaca <tommygodoybaca@outlook.com>
2025-03-04 14:28:29 -0600

    added fuctionality to the notes - added new note button so new notes can be created - when making a new note the user automatically gets routed to the new note - added button to delete notes - added function to grab all note keys when the app is loaded - minor styling

--- app/(tabs)/notes/index.js
+++ app/(tabs)/notes/index.js
@@ -1,35 +1,113 @@
-import { Text, View, StyleSheet, ScrollView, } from 'react-native';
-import { Link } from 'expo-router';
+import { Text, View, StyleSheet, ScrollView, Button } from 'react-native';
+import { useLocalSearchParams, useGlobalSearchParams, Link, router } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from 'react';
 
 
 const noteObject = {
     1234: { name: "Note 1", content: "This is my first note, and it might not work." },
-    14284: { name: "Note 2", content: "This is my second note, I dont know if it will work." }
+    14284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14484: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1428464: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1424584: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1423284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1476284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1421284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14285434: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1423284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14345284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1454284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14234284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14463284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1434284: { name: "Note 981", content: "Will this one work?" },
+    14223484: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1465284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14243284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    144643284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
 }
 
 export default function NotesScreen() {
+    //used to refresh page when button is pressed.
+    const [buttonPressed, ifButtonPressed] = useState(false);
+    //keys of all of the notes saved
+    const [noteKeys, refreshNoteKeys] = useState([]);
 
-    let noteList = [];
+    //creates new note when the new note button is pressed.
+    const createNewNote = async () => {
+        ifButtonPressed(!buttonPressed);
+        try {
+            let noteId = Date.now().toString();
+            const jsonValue = JSON.stringify({ name: "New Note", content: "This is a new note." });
+            await AsyncStorage.setItem(noteId, jsonValue);
+            router.navigate(`notes/note?noteId=${noteId}`);
+        } catch (e) {
+            // saving error
+        }
+    };
 
-    for (let i in noteObject) {
-        noteList = [...noteList, <Link key={i} href="notes/note">{noteObject[i].name}</Link>]
+    async function deleteNote(noteId) {
+        try {
+            await AsyncStorage.removeItem(noteId);
+            ifButtonPressed(!buttonPressed);
+        } catch (e) {
+            // remove error
+        }
+
+        console.log(noteId)
     }
 
+    useEffect(() => {
+        async function getAllKeys() {
+            let keys = []
+            try {
+                keys = await AsyncStorage.getAllKeys()
+                refreshNoteKeys(keys);
+            } catch (e) {
+                console.log(e)
+            }
+            // example console.log result:
+            // ['@MyApp_user', '@MyApp_key']
+        }
+        getAllKeys();
+    }, [buttonPressed]);
+
+
     return (
-        <ScrollView style={styles.container}>
-            <Text style={styles.text}>Notes Home Screen</Text>
-            <Link href="notes/note" style={styles.button}>
-                Go to Details screen
-            </Link>
-            <View>{noteList}</View>
-        </ScrollView>
+        <View style={styles.container}>
+            <ScrollView>
+                <View style={styles.displayNotes}>
+                    {noteKeys.map((data) => {
+                        return (
+                            <View style={styles.individualNotes} key={data}>
+                                <Link href={`notes/note?id=${data}`} style={[styles.individualNotes, styles.text]}>{data}</Link>
+                                <Button
+                                    onPress={() => { deleteNote(data) }}
+                                    title="Delete Note"
+                                    color="#841584"
+                                    accessibilityLabel="Make a new note."
+                                />
+                            </View>
+                        )
+                    })};
+                </View>
+
+                {/* <View style={styles.displayNotes}>{noteList}</View> */}
+            </ScrollView>
+            <View style={styles.newNoteButton}>
+                <Button
+                    onPress={createNewNote}
+                    title="New Note"
+                    color="#841584"
+                    accessibilityLabel="Make a new note."
+                />
+            </View>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        margin: 15,
     },
     text: {
         color: '#424940',
@@ -38,5 +116,25 @@ const styles = StyleSheet.create({
         color: '#424940',
         fontSize: 20,
         textDecorationLine: 'underline',
+    },
+    displayNotes: {
+        display: 'flex',
+
+    },
+    individualNotes: {
+        borderBottomWidth: 2,
+        padding: 10,
+        borderColor: '#42494080',
+        fontSize: 18,
+    },
+    newNoteButton: {
+        display: 'flex',
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        margin: 10,
+        justifyContent: 'center',
+        alignContent: 'center',
+        backgroundColor: 'grey',
     }
 });
--- app/(tabs)/notes/note.js
+++ app/(tabs)/notes/note.js
@@ -1,11 +1,50 @@
 import { Text, View, StyleSheet, TextInput, ScrollView } from 'react-native';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useLocalSearchParams } from 'expo-router';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 
+
+const noteObject = {
+    1234: { name: "Note 1", content: "This is my first note, and it might not work." },
+    14284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14484: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1428464: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1424584: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1423284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1476284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1421284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14285434: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1423284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14345284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1454284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14234284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14463284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1434284: { name: "Note 981", content: "Will this one work?"},
+    14223484: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    1465284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    14243284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+    144643284: { name: "Note 2", content: "This is my second note, I dont know if it will work." },
+}
 
 export default function NotesScreen() {
-    const [title, onChangeTitle] = useState("Placeholder Title");
-    const [text, onChangeText] = useState("Placeholder Text");
+    const { noteId } = useLocalSearchParams();
 
+    useEffect(() => {
+        async function getNote(noteID) {
+            let keys = []
+            try {
+                keys = await AsyncStorage.getItem(noteID)
+            } catch (e) {
+                console.log(e)
+            }
+            // example console.log result:
+            // ['@MyApp_user', '@MyApp_key']
+        }
+        getNote(noteId);
+    }, []);
+
+    const [title, onChangeTitle] = useState("Note Title");
+    const [text, onChangeText] = useState("Write Here");
 
     return (
         <ScrollView style={styles.container}>
@@ -14,7 +53,7 @@ export default function NotesScreen() {
                 onChangeText={onChangeTitle}
                 value={title}
             />
-            <Text style={[styles.text, styles.date]}>03, March, 2025</Text>
+            <Text style={[styles.text, styles.date]}>{new Date(Number(noteId)).toString()}</Text>
             <TextInput
                 style={styles.text}
                 onChangeText={onChangeText}
@@ -37,11 +76,11 @@ const styles = StyleSheet.create({
         fontSize: 28,
         borderBottomWidth: 2,
         borderBottomColor: '#42494050',
-        marginBottom: 10,
-        paddingBottom: 10,
+        marginBottom: 4,
+        paddingBottom: 4,
     },
     date: {
-        fontSize: 18,
+        fontSize: 16,
         color: '#42494075',
         paddingLeft: 20,
     }
